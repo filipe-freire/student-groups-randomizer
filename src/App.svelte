c8d5c31522585aad9e5c83d2829c19784feead73
Filipe <filipefreire@protonmail.com>
2021-08-11 15:52:36 +0200

    Added main features

--- src/App.svelte
+++ src/App.svelte
@@ -2,37 +2,15 @@
   function generateRandomNumber(lower: number, upper: number) {
     return Math.floor(Math.random() * (upper + 1 - lower)) + lower;
   }
-  // const students = [
-  //   "Marie",
-  //   "Cheryl",
-  //   "Anna",
-  //   "Amy",
-  //   "Daniel",
-  //   "Juan",
-  //   "Noemi",
-  //   "Chris",
-  //   "Pablo",
-  // ];
-  const students = [];
-
-  let studentsInput = "";
-
-  function createClass() {
-    const value = studentsInput.split(",").map((student) => {
-      const normalizedName = student.trim();
-      return normalizedName[0].toLocaleUpperCase() + normalizedName.slice(1);
-    });
-    value.forEach((el) => students.push(el));
-  }
-
-  const groups = [];
-
-  function createGroups(numOfPeople) {
+  // Creates groups of 2 people by default
+  function createGroups() {
+    if (!students.length || numOfMembers <= 0) return;
     const studentsCopy = [...students];
+    const finalGroups = [];
 
     while (studentsCopy.length > 0) {
       const group = [];
-      for (let i = 0; i < numOfPeople; i++) {
+      for (let i = 0; i < numOfMembers; i++) {
         const randomIndex = generateRandomNumber(0, studentsCopy.length - 1);
         if (studentsCopy.length) {
           group.push(studentsCopy[randomIndex]);
@@ -41,38 +19,75 @@
           break;
         }
       }
-      groups.push(group);
+      finalGroups.push(group);
+    }
+    groups = [...finalGroups];
+    console.log(groups);
+  }
+  function createClass() {
+    const value = studentsInput.split(",").map((student) => {
+      const normalizedName = student.trim();
+      return normalizedName[0].toLocaleUpperCase() + normalizedName.slice(1);
+    });
+    students = [...students, ...value];
+  }
+  function handleReturn(e) {
+    if (e.code === "Enter") {
+      createClass();
+      studentsInput = "";
     }
   }
-
-  // createGroups(2);
+  let studentsInput = "";
+  let students = [];
+  let groups = [];
+  let numOfMembers = 2;
 </script>
 
 <main>
   <h1>Group Randomizer!</h1>
 
   <h2>Students</h2>
-  <label for="addStudent">Add Students</label>
+  <label for="addStudent">Student(s)</label>
   <input
+    style="width: 200px;"
     id="addStudent"
     type="text"
     placeholder="student1, student2, student3..."
     bind:value={studentsInput}
+    on:keyup={(e) => handleReturn(e)}
   />
-  <button on:click={() => createClass()}>Create Class</button>
+  <button on:click={() => createClass()}>Add Student(s)</button>
+  <button
+    on:click={() => {
+      students.length = 0;
+    }}>Reset</button
+  >
   <br />
-  <button on:click={() => createGroups(2)}>Create groups</button>
+  {#if students.length > 1}
+    <label for="numOfMembers"
+      >How many members do you want the groups to have?</label
+    >
+    <input id="numOfMembers" bind:value={numOfMembers} type="number" />
+    <button on:click={() => createGroups()}>Create groups</button>
+  {/if}
 
   <!-- Students on the list -->
   <h2>List of students:</h2>
-  <ol>
-    {#each students as student}
-      <li>{student}</li>
-    {/each}
-  </ol>
+  {#if students.length === 0}
+    <p>You're class is empty, please add students.</p>
+  {:else}
+    <ol>
+      {#each students as student}
+        <li>{student}</li>
+      {/each}
+    </ol>
+  {/if}
 
   <!-- final groups -->
+  {#if groups.length}
+    <h2>Final Groups</h2>
+  {/if}
   {#each groups as group, i}
-    <p><b>Group {i + 1}</b> - {group.join(" & ")}</p>
+    <p><b>Group {i + 1}</b>: {group.join(" - ")}</p>
   {/each}
 </main>
