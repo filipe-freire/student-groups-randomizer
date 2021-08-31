b199377d4e47214da30dbb95558e248f16629e38
Filipe <filipefreire@protonmail.com>
2021-08-31 18:19:06 +0200

    add: text-shadow to button text

--- src/App.svelte
+++ src/App.svelte
@@ -189,6 +189,8 @@
     border-radius: 5px;
     text-transform: uppercase;
     letter-spacing: 1.8px;
+    text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.63);
+    line-height: 2ch;
     box-shadow: rgba(17, 17, 26, 0.05) 0px 1px 0px,
       rgba(17, 17, 26, 0.1) 0px 0px 8px;
     cursor: pointer;
