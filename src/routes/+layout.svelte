02de9c0527c6b1f0f2e3a54def0087a4af55cdc6
Filipe <filipefreire@protonmail.com>
2024-10-12 17:31:10 +0200

    chore: bump deps & document prod bug

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,6 +12,12 @@
 
 <label class="block pt-2 pl-4" for="language">
 	{$t('languageLabel')}
+	<!-- 
+	🐛 There is a bug in Prod, where the lang is loaded from LocalStorage but the select 
+	input's state does not update accordingly, even though the locales are set correctly. 
+
+	Does not happen on dev.
+	-->
 	<select
 		class="bg-blue-100 border border-blue-300 text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block p-2.5"
 		bind:value={$locale}
