5be8c354b063b8b9a76529ed5620922b620f4f9b
Filipe <filipefreire@protonmail.com>
2024-10-12 18:25:16 +0200

    fix: select input not reflecting localStorage locale

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,21 +12,18 @@
 
 <label class="block pt-2 pl-4" for="language">
 	{$t('languageLabel')}
-	<!-- 
-	🐛 There is a bug in Prod, where the lang is loaded from LocalStorage but the select 
-	input's state does not update accordingly, even though the locales are set correctly. 
-
-	Does not happen on dev.
-	-->
 	<select
 		class="bg-blue-100 border border-blue-300 text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block p-2.5"
 		bind:value={$locale}
 		name="language"
 		id="language"
-		onchange={() => localStorage.setItem('lang', $locale)}
+		onchange={() => {
+			localStorage.setItem('lang', $locale);
+			locale.set($locale);
+		}}
 	>
 		{#each locales as l}
-			<option value={l}>{l}</option>
+			<option selected={$locale === l} value={l}>{l}</option>
 		{/each}
 	</select>
 </label>
